e2d069f7a1385e622351b5f27a1a5c37cb3b62af
harugasumi-works <tokino.yanagi@gmail.com>
2026-09-24 19:57:34 +0900

    Hot fix (2)
    - Clean up build.gradle.kts

--- StatusCheck/lib/build.gradle.kts
+++ StatusCheck/lib/build.gradle.kts
@@ -22,12 +22,6 @@ dependencies {
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
     
     implementation("tools.jackson.core:jackson-databind:3.2.2")
     implementation("tools.jackson.dataformat:jackson-dataformat-csv:3.2.2")
